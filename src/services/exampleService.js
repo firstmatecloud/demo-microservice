7b17f43f84b2e857b72d401755ae3e47e443f0b3
wvl94 <ward.vanlaer@hotmail.com>
2024-08-28 20:52:33 +0200

    add api call in service

--- src/services/exampleService.js
+++ src/services/exampleService.js
@@ -1,9 +1,22 @@
 import exampleRepo from "../repos/exampleRepo.js"
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: appConfig.aiServiceUrl(),
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
 
 export class ExampleService {
 
     async getById(id) {
+        const result = await api.post("/analyse", {
+            id,
+        });
+        console.log(result.data);
         return await exampleRepo.getById(id)
+
     }
 
 
