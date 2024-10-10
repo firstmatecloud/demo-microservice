4a630e3a0e20c802bb471088017742236c746fbf
desyco <jochen.wilms@hyfen.nl>
2024-10-10 09:59:38 +0200

    Add log statement.

--- src/services/exampleService.js
+++ src/services/exampleService.js
@@ -1,12 +1,13 @@
 import exampleRepo from "../repos/exampleRepo.js"
+import logger from "../utils/logger.js";
 
 export class ExampleService {
 
     async getById(id) {
-        return await exampleRepo.getById(id)
+        const userDetails = await exampleRepo.getById(id)
+        logger.info({userDetails}, "UserDetails object retrieved")
+        return userDetails
     }
-
-
 }
 
 export default new ExampleService();
